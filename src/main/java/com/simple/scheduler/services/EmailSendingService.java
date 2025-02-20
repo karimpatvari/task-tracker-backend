c0132248ea17df4749e8d903bd8e9c0c73a78513
Karim <bungarik@gmail.com>
2025-02-19 18:04:14 -0600

    Initial commit

---
+++ src/main/java/com/simple/scheduler/services/EmailSendingService.java
@@ -0,0 +1,20 @@
+package com.simple.scheduler.services;
+
+
+import com.common.EmailSendingDto;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSendingService {
+
+    private final KafkaTemplate<String, EmailSendingDto> kafkaTemplate;
+
+    public EmailSendingService(KafkaTemplate<String, EmailSendingDto> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void SendEmail(EmailSendingDto task) {
+        kafkaTemplate.send("EMAIL_SENDING_TASKS", task);
+    }
+}
